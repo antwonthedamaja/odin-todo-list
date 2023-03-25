7de64debe21d130b4a0f3d21ae2f7b5eec66a767
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-25 13:20:53 -0400

    Code note creation functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './style.css';
 import {list} from './list.js';
+import {handleDOM} from './list.js';
 
 const modal = document.querySelector('#modal');
 const openModal = document.querySelector("#add-item");
-const closeModal = document.querySelector("#close-modal");
+const closeModal = document.querySelectorAll(".close-modal");
 
 openModal.addEventListener('click', () => modal.showModal());
-closeModal.addEventListener('click', () => modal.close());
+closeModal.forEach(button => {
+    button.addEventListener('click', () => modal.close());
+});
 
 const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', () => {
-    
-})
+    list.addItem();
+    handleDOM.renderList();
+});
--- src/list.js
+++ src/list.js
@@ -1,20 +1,38 @@
-function elementFactory(type, class1, class2, id, text) {
-    const element = document.createElement(type);
-    if (class1) element.classList.add(class1);
-    if (class2) element.classList.add(class2);
-    if (id) element.setAttribute("id", id);
-    if (text) element.innerHTML = text;
-    return element;
+function elementFactory(element, type, class1, class2, id, text) {
+    const newElement = document.createElement(element);
+    if (type) newElement.setAttribute('type', type);
+    if (class1) newElement.classList.add(class1);
+    if (class2) newElement.classList.add(class2);
+    if (id) newElement.setAttribute("id", id);
+    if (text) newElement.innerHTML = text;
+    return newElement;
 }
 
 export const list = {
     noteList: [],
-    renderList: function() {
-        this.noteList.forEach(note => {
-
-        });
+    BookObject: function(note, date) {
+        this.note = note;
+        this.date = date;
+        this.check = false;
     },
     addItem: function() {
-        const newNote = {};
+        const note = document.querySelector('#note');
+        const date = document.querySelector('#date');
+        const newNote = new this.BookObject(note.value, date.value, false);
+        this.noteList.push(newNote); 
+    },
+};
+
+export const handleDOM = {
+    renderList: function() {
+        const items = document.querySelector('#items');
+        list.noteList.forEach((item, index) => {
+            items.appendChild(elementFactory('div', undefined, 'item', undefined, `item${index}`, undefined));
+            items.lastChild.appendChild(elementFactory('input', 'checkbox', undefined, undefined, undefined, undefined));
+            items.lastChild.appendChild(elementFactory('div', undefined, 'note', undefined, undefined, item.note));
+            items.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, undefined, undefined));
+            items.lastChild.lastChild.appendChild(elementFactory('div', undefined, 'date', undefined, undefined, item.date));
+            items.lastChild.lastChild.appendChild(elementFactory('button', 'button', undefined, undefined, undefined, 'X'));
+        });
     },
 };
