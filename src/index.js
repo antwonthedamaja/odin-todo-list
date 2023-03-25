b417b1d2e3add2dcfd2caff7baea2ac93bdc8b4a
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-24 21:53:50 -0400

    Create and style list UI and modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector("#add-item");
+const closeModal = document.querySelector("#close-modal");
+
+openModal.addEventListener('click', () => modal.showModal());
+closeModal.addEventListener('click', () => modal.close());
---
+++ src/list.js
@@ -0,0 +1,6 @@
+export const list = {
+    noteList: [],
+    addItem: function() {
+
+    },
+};
